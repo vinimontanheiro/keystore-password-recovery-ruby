bd3d073bc4124533a8b9b289062e166882e3f9db
Vinícius Montanheiro <vinicius.monranheiro@utech.com.br>
2019-08-22 09:26:57 -0300

    First commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+
+# Android Keystore Password Recovery
+
+## Dependencies:
+
+- [Ruby](https://www.ruby-lang.org/en/documentation/installation/)
+
+#### To run
+```
+$ cd project-folder
+$ ruby main.rb path/your-file.keystore 
+```
+
+#### or use a custom dictionary
+```
+$ cd project-folder
+$ ruby main.rb path/your.keystore path/your-dictionary-file 
+```
+
+#### The default dictionary has about 4500000 of words
+#### The result will saved in result.txt file
+
+
+### Don't lose hope, good lucky :D
+
+
+
---
+++ main.rb
@@ -0,0 +1,24 @@
+$keystore_path = ARGV[0]
+$dictionary_path = ARGV[1] ? ARGV[1] : 'dictionary.txt'
+
+puts 'Starting...'
+
+if $keystore_path.nil?
+  puts "The keystore path was not specified!!"
+  abort
+end
+
+# file=File.open($dictionary_path).read
+# file.gsub!(/\r\n?/, "\n")
+# file.each_line do |line|
+#   puts "Trying word >> #{line}"
+#   output = system "keytool -list -keystore #{$keystore_path} -storepass #{line}"
+#   if output && output != false
+#     result = "Found >> #{line}"
+#     puts result
+#     File.open('result.txt', 'w') { |file| 
+#       file.write(result)
+#     }
+#     abort
+#   end
+# end
