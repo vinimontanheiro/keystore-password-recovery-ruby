f657107ee07d8f20b6ab8f1902cbca7879c3d55c
Vinícius Montanheiro <vinicius.amontanheiro@gmail.com>
2019-08-22 21:28:17 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -8,17 +8,17 @@ if $keystore_path.nil?
   abort
 end
 
-# file=File.open($dictionary_path).read
-# file.gsub!(/\r\n?/, "\n")
-# file.each_line do |line|
-#   puts "Trying word >> #{line}"
-#   output = system "keytool -list -keystore #{$keystore_path} -storepass #{line}"
-#   if output && output != false
-#     result = "Found >> #{line}"
-#     puts result
-#     File.open('result.txt', 'w') { |file| 
-#       file.write(result)
-#     }
-#     abort
-#   end
-# end
+file=File.open($dictionary_path).read
+file.gsub!(/\r\n?/, "\n")
+file.each_line do |line|
+ puts "Trying word >> #{line}"
+ output = system "keytool -list -keystore #{$keystore_path} -storepass #{line}"
+ if output && output != false
+   result = "Found >> #{line}"
+   puts result
+   File.open('result.txt', 'w') { |file| 
+     file.write(result)
+   }
+   abort
+ end
+end
